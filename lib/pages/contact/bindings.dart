3c5efa4ef3ae525a49e7347c76742a0f953c8241
thongpq <thongpq@d-soft.com.vn>
2023-02-04 12:15:30 +0700

    add

---
+++ lib/pages/contact/bindings.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_chat/pages/sign_in/controller.dart';
+import 'package:firebase_chat/pages/welcomes/controller.dart';
+import 'package:get/get.dart';
+
+class SignInBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<SignInController>(() => SignInController());
+  }
+
+}
